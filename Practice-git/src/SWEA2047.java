7e5830fe538917c8b5cb31645a67c51c2fc6a514
dmswls4634 <69571885+dmswls4634@users.noreply.github.com>
2022-11-16 17:21:33 +0900

    221116

---
+++ Practice-git/src/SWEA2047.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA2047 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		System.out.print(input.toUpperCase());
+	}
+
+}
