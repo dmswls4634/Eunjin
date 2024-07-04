d6e3426a6f08e8877f1f9e121ea94fb0f7544632
dmswls4634 <69571885+dmswls4634@users.noreply.github.com>
2024-07-04 13:13:06 +0900

    react tictactoe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState}from'react';
+//import React,{StrictMode} from 'react';
+//import {createRoot} from 'react-dom/client';
+//import App from './App';
+
+/*const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>
+);*/
+
+function Square({value,onClick}){
+  return <button className='square' onClick={onClick}>{value}</button>
+}
+
+function Board(){
+  const [xIsNext, setxIsNext]=useState(true);
+  const [squares, setSquares]=useState(Array(9).fill(null))
+  
+  function Winner(squares){
+    const lines=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    for(let i=0;i<lines.length;i++){
+      const [a,b,c]=lines[i];
+      if(squares[a]&&squares[a]===squares[b] && squares[a] === squares[c]){
+        console.log("You're Winner! Congraturation~!");
+        return squares[a]
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i){
+
+    if(squares[i]||Winner(squares)){
+      return
+    }
+
+    const nextSquares = squares.slice();
+    nextSquares[i]=xIsNext ? 'X':'O';
+    setSquares(nextSquares);
+    setxIsNext(!xIsNext);
+  }
+  console.log(squares)
+
+  return(
+    <>
+    <div className='board-row'>
+      <Square value={squares[0]} onClick={()=>handleClick(0)}></Square>
+      <Square value={squares[1]} onClick={()=>handleClick(1)}></Square>
+      <Square value={squares[2]} onClick={()=>handleClick(2)}></Square>
+    </div>
+    <div className='board-row'>
+      <Square value={squares[3]} onClick={()=>handleClick(3)}></Square>
+      <Square value={squares[4]} onClick={()=>handleClick(4)}></Square>
+      <Square value={squares[5]} onClick={()=>handleClick(5)}></Square>
+    </div>
+    <div className='board-row'>
+      <Square value={squares[6]} onClick={()=>handleClick(6)}></Square>
+      <Square value={squares[7]} onClick={()=>handleClick(7)}></Square>
+      <Square value={squares[8]} onClick={()=>handleClick(8)}></Square>
+    </div>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Board/>
+    </>
   );
 }
 
